6a12962691e501ca619347ff365da5cdfc6b4986
Rodrigo Ziviani de Souza <rodrigo.ziviani@gmail.com>
2017-09-03 17:59:10 +1200

    Added CrossOrigin configuration to allow me to use a local angular for front end

--- src/main/java/com/rodrigoziviani/tasklist/TaskRepository.java
+++ src/main/java/com/rodrigoziviani/tasklist/TaskRepository.java
@@ -2,6 +2,8 @@ package com.rodrigoziviani.tasklist;
 
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @Repository
+@CrossOrigin(origins = "http://localhost:4200")
 public interface TaskRepository extends PagingAndSortingRepository<Task, Long> { }
